eb8eb726c1dca86ba58a4cba1391309f7afd1415
Sarun Olankranok <Sarun.Olan@gmail.com>
2025-03-18 11:33:00 +0700

    resize video, layout for mobile, mobile menu

--- src/components/Menubar.tsx
+++ src/components/Menubar.tsx
@@ -1,4 +1,13 @@
-const DEFAULT_MENU: string[] = [
+import { Menu } from "lucide-react";
+import { useState } from "react";
+type MENU_TYPE =
+  | "HOME"
+  | "ABOUT"
+  | "PROJECT"
+  | "SKILLS"
+  | "EXPERIENCE"
+  | "CONTACT";
+const DEFAULT_MENU: MENU_TYPE[] = [
   "HOME",
   "ABOUT",
   "PROJECT",
@@ -6,17 +15,68 @@ const DEFAULT_MENU: string[] = [
   "EXPERIENCE",
   "CONTACT",
 ];
+
 const Menubar = () => {
+  const [currentPage, setCurrentPage] = useState<MENU_TYPE>("HOME");
+  const [isOpenMenuMobile, setIsOpenMenuMobile] = useState(false);
+  const handleCloseMenu = () => {
+    setIsOpenMenuMobile(false);
+  };
+  const handleOpenMenu = () => {
+    setIsOpenMenuMobile(true);
+  };
+  const handleSelectPage = (page: MENU_TYPE) => {
+    setCurrentPage(page);
+  };
   return (
-    <div className="fixed left-0 top-0 h-14 w-full  flex items-center px-4 justify-between mix-blend-difference z-10">
+    <div
+      className={`fixed left-0 top-0 h-14 w-full  flex items-center px-4 justify-between ${
+        isOpenMenuMobile ? "" : "mix-blend-difference"
+      } z-10`}
+    >
+      {isOpenMenuMobile && (
+        <div
+          className="fixed left-0 top-0 w-full h-full"
+          onClick={() => handleCloseMenu()}
+        >
+          <div
+            className="fixed left-0 bottom-0 h-11/12 bg-[rgba(255,255,255,0.9)] w-full border border-[rgba(209,213,219,1)] rounded-t-xl z-50 flex flex-col justify-evenly items-center text-5xl cursor-pointer"
+            onClick={(event) => {
+              event.stopPropagation();
+            }}
+          >
+            {DEFAULT_MENU.map((menu) => {
+              return (
+                <div
+                  className=" text-black"
+                  onClick={() => {
+                    handleSelectPage(menu);
+                  }}
+                >
+                  {menu}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
       <div className="font-bold cursor-pointer">RUN</div>
-      <div className="flex [&>div]:ml-3">
+      <div
+        className="inline cursor-pointer md:hidden"
+        onClick={() => handleOpenMenu()}
+      >
+        <Menu />
+      </div>
+      <div className="hidden md:flex [&>div]:ml-3">
         <div className="cursor-default">[</div>
         {DEFAULT_MENU.map((menu) => {
           return (
             <div
               className="cursor-pointer relative after:content-[''] after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-[1px] after:transition-[width] after:duration-500 after:ease-in-out hover:after:w-full hover:after:bg-white hover:font-bold hover:text-lg duration-500"
               key={menu}
+              onClick={() => {
+                handleSelectPage(menu);
+              }}
             >
               {menu}
             </div>
--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -1,36 +1,48 @@
+import { FileDown, Github, Linkedin } from "lucide-react";
 import Layout from "./Layout";
-import start from "../assets/start.mp4";
-
+import homeVideo from "../assets/home-video.mp4";
 const Home = () => {
   return (
     <Layout>
       <div>
-        <div className="absolute left-0 top-0">
-          <video autoPlay loop muted playsInline>
-            <source src={start} type="video/mp4" />
+        <div className="absolute left-0 top-0 w-full h-full overflow-hidden inset-0">
+          <video
+            autoPlay
+            loop
+            muted
+            playsInline
+            className="h-full w-full object-cover"
+          >
+            <source src={homeVideo} type="video/mp4" />
           </video>
         </div>
         <div className="absolute right-4 top-1/3 w-[calc(50%-16px)] mix-blend-difference">
-          <div className="flex items-end">
-            <div className="text-8xl font-bold mr-3">SARUN</div>
-            <div className=" font-light">FULLSTACK DEVELOPER</div>
+          <div className="lg:flex lg:items-end">
+            <div className="text-2xl font-bold mr-3 lg:text-8xl ">SARUN</div>
+            <div className=" font-light text-xs">FULLSTACK DEVELOPER</div>
           </div>
-          <div className="text-8xl font-bold">OLANKRANOK</div>
-          <div className="bg-[rgba(255,255,255,0.1)] w-full border border-[rgba(209,213,219,0.3)] rounded-xl p-5 backdrop-blur-md">
+          <div className="text-2xl font-bold lg:text-8xl">OLANKRANOK</div>
+          <div className="text-sm bg-[rgba(255,255,255,0.1)] w-full border border-[rgba(209,213,219,0.3)] rounded-xl p-5 backdrop-blur-md lg:text-base">
             Building modern, interactive, and responsive web applications with
             React, TypeScript, and cutting-edge UI/UX design.
           </div>
-          <div className="flex justify-between mt-3 [&>div]:flex [&>div]:rounded-xl [&>div]:border [&>div]:border-[rgba(209,213,219,0.3)] [&>div]:hover:bg-[rgba(255,255,255,0.1)] [&>div]:py-3 [&>div]:px-14 [&>div]:cursor-pointer">
+          <div className="lg:flex lg:justify-between [&>div]:mt-3  [&>div]:flex [&>div]:rounded-xl [&>div]:border [&>div]:border-[rgba(209,213,219,0.3)] [&>div]:hover:bg-[rgba(255,255,255,0.1)] [&>div]:py-3 [&>div]:px-14 [&>div]:cursor-pointer">
             <div className="">
-              <div></div>
+              <div className="mr-1">
+                <FileDown />
+              </div>
               <div>Resume</div>
             </div>
             <div>
-              <div></div>
+              <div className="mr-1">
+                <Github />
+              </div>
               <div>Github</div>
             </div>
             <div>
-              <div></div>
+              <div className="mr-1">
+                <Linkedin />
+              </div>
               <div>Linkedin</div>
             </div>
           </div>
